cdbde3fd3963849b789b063c3aeb11e5935e2f4f
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-06-11 21:54:16 -0600

    Creando la clase cliente y sus set y get

---
+++ src/agregacioncomposicion/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package agregacioncomposicion;
+
+/**
+ *
+ * @author PC
+ */
+public class Cliente {
+    private String nombre;
+    public Cliente(String nombre){
+        this.nombre = nombre;
+    }
+    
+    void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    String getNombre(){
+        return nombre;
+    }
+}
